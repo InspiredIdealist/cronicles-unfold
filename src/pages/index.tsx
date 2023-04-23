76526af55b2c39e6890e06dedd1f587461e325ba
Moran, Lucas <lucas.moran40@gmail.com>
2023-04-23 14:31:33 -0400

    added prompt bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Expander, ExpanderItem } from '@aws-amplify/ui-react';
+import { Button, Divider, Expander, ExpanderItem, FieldGroupIcon, Flex, TextField, View } from '@aws-amplify/ui-react';
 import { getPosts } from './api/posts';
 
 export async function getStaticProps() {
@@ -15,8 +15,41 @@ export default function Home({ posts }: any) {
     ));
 
     return (
-        <Expander type="multiple">
-            {frags}
-        </Expander>
+        <>
+            <Flex direction="column" gap="8rem">
+                <div>
+                    <Expander type="multiple">
+                        {frags}
+                    </Expander>
+                    <Divider orientation='horizontal' label="current" />
+                </div>
+                <div />
+                <View
+                    className="bottom-0 w-full fixed"
+                    backgroundColor="var(--amplify-colors-white)"
+                    borderRadius="6px"
+                    border="1px solid var(--amplify-colors-black)"
+                    boxShadow="3px 3px 5px 6px var(--amplify-colors-neutral-60)"
+                    color="var(--amplify-colors-blue-60)"
+                    maxWidth="100%"
+                    padding="1rem"
+                >
+                    <TextField
+                        label=""
+                        type="text"
+                        labelHidden={true}
+                        placeholder="what happened next?"
+                        innerStartComponent={
+                            <FieldGroupIcon>
+                                📇
+                            </FieldGroupIcon>
+                        }
+                        outerEndComponent={
+                            <Button onClick={() => { }}>Send</Button>
+                        }
+                    />
+                </View>
+            </Flex>
+        </>
     );
 }
