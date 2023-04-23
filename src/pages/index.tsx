9166c431e0f8de24122b54fb2cd48a6bdf2a7b46
Moran, Lucas <lucas.moran40@gmail.com>
2023-04-23 09:27:13 -0400

    woops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Expander, ExpanderItem } from '@aws-amplify/ui-react';
 export default function Home() {
 
     const frags = Array(100).fill(0).map((_, i) => (
-        <ExpanderItem value={i} title={"NUMBA " + i}>
+        <ExpanderItem value={i.toString()} key={i} title={`NUMBA ${i}`}>
             {i}
         </ExpanderItem>
     ));
